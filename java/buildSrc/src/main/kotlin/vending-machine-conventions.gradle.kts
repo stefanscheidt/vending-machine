a0a3e0015d5c161f32d81c26246828edc06af85d
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2023-08-25 15:47:18 +0200

    Add JUnit launcher to test runtime classpath

--- java/buildSrc/src/main/kotlin/vending-machine-conventions.gradle.kts
+++ java/buildSrc/src/main/kotlin/vending-machine-conventions.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     testImplementation("org.assertj:assertj-core:3.24.2")
 }
